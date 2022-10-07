5bdf42da0eda1a537cb45a9403f9814dee3adbeb
Welber33 <jwwork21@gmail.com>
2022-10-07 11:17:50 -0300

    Building Video Server Side Data and Fetching Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
+import { Video } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  videos: Video[]
+}
+
+const Home = ({ videos }: Props) => {
+  console.log(videos)
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
+}
+
+export async function getServerSideProps(){
+  const { data }  = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
